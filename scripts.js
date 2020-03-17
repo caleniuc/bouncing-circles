af82bafde49fa07ea1526bfe3a30fd8ccbc1e5e9
Andrei <andrei_caleniuc@live.com>
2020-03-17 18:13:48 +0200

    Spawn given nr of circles

--- scripts.js
+++ scripts.js
@@ -1,3 +1,31 @@
+function getCtx() {
+    const c = document.getElementById("my-canvas");
+    return c.getContext("2d");
+}
+
+const ctx = getCtx();
+
+function clearCanvas() {
+    const c = document.getElementById("my-canvas")
+    ctx.clearRect(0, 0, c.width, c.height);
+}
+
+function Ball() {
+    this.x = getRandom(1, 500);
+    this.y = getRandom(1, 300);
+    this.velX = getRandom(0, 10);
+    this.velY = getRandom(0, 10);
+    this.color = getRandomRGB();
+    this.radius = getRandom(10, 30);
+}
+
+Ball.prototype.draw = function() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+}
+
 function getRandom(bottomLimit, upperLimit) {
     return Math.floor(Math.random() * upperLimit) + bottomLimit;
 }
@@ -9,17 +37,17 @@ function getRandomRGB() {
   return "rgb(" + r + "," + g + "," + b + ")";
 }
 
-function drawCircle() {
-    const c = document.getElementById("my-canvas");
-    var ctx = c.getContext("2d");
-    ctx.beginPath();
-    const rndX = getRandom(1, 500);
-    const rndY = getRandom(1, 350);
-    const rndSize = getRandom(10, 30);
-    ctx.arc(rndX, rndY, rndSize, 0, 2 * Math.PI);
-    ctx.fillStyle = getRandomRGB();
-    ctx.fill();
+function spawnCircles() {
+    clearCanvas();
+    const input = document.getElementById('circles-nr');
+    const nrOfCircles = input.value;
+    let i = 0;
+    while(i < nrOfCircles) {
+        const ball = new Ball();
+        ball.draw();
+        i++;
+    }
 }
 
 const spawnBtn = document.getElementById('spawn-btn');
-spawnBtn.addEventListener('click', drawCircle);
+spawnBtn.addEventListener('click', spawnCircles);
